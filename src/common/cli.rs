1cb464b23dd9893f72346bb90b693aebe67719ec
Karthik Venugopal <kavi.develog@gmail.com>
2024-09-21 12:48:44 -0400

    Remove extra files

--- src/common/cli.rs
+++ src/common/cli.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 use argh::FromArgs;
 
 #[derive(FromArgs)]
--- src/types/ptr_graph.rs
+++ src/types/ptr_graph.rs
@@ -1,5 +1,3 @@
-#![allow(dead_code)]
-
 use std::rc::Rc;
 use std::cell::RefCell;
 use std::vec::Vec;
@@ -30,7 +28,7 @@ impl<T: PtrGraphValue> PtrGraphNode<T> {
 }
 
 // Any way to not have to write the entire type qualification each time?
-impl<T: PtrGraphValue> PartialEq for PtrGraphNode<T> {
+impl<T> PartialEq for PtrGraphNode<T> where T : PtrGraphValue {
     fn eq(&self, other: &Self)-> bool {
         self.value==other.value
     }
@@ -44,7 +42,7 @@ pub(crate) fn create_ptr_graph<T>() -> PtrGraph<T> where T : PtrGraphValue {
         graph.push(PtrGraphNode::<T>::new(i, T::default()));
     }
 
-    let mut _edge_lists: Vec::<Vec::<usize>> = vec![
+    let _edge_lists: Vec::<Vec::<usize>> = vec![
         vec![1, 2], vec![0, 3, 4], vec![0, 5], vec![1, 5], vec![1],
         vec![2, 3, 6], vec![5, 9], vec![5, 8], vec![7], vec![6, 3]
     ];
